3a7e138653489e957d787befab052fb32e4a309c
Raunak Singh <raunaksingh@Raunaks-Air.lan>
2025-02-08 20:46:00 +0530

    homepage almost done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,58 +1,29 @@
-
 import ColourfulText from "@/components/ui/colourful-text";
-import { ImageH}  from "@/components/Image/image";
+import { ImageH } from "@/components/Image/image";
 import DevfolioBtn from "@/components/applyDevFolio/btn";
 
-
-
+import { BackgroundGradientAnimation } from "@/components/ui/background-gradient-animation";
+import { div, main } from "motion/react-client";
 
 export default function Home() {
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center text-3xl">
-      <div className="mt-5 mr-5 absolute top-0 right-0  justify-center items-center gap-2 p-2 rounded-md">
-        <DevfolioBtn />
-      </div>
-      <div className="w-fit h-fit flex flex-row justify-center items-center text-3xl">
-        <div className=" opacity-45 font-medium">Coming Soon</div>
-        <div className=" font-bold">
-          <ColourfulText text=" Cepheus" />
-        </div>
-      </div>
-      <div className=" text-xs mt-4 opacity-40">
-        By GDG · On Campus{" "}
-        <a
-          href="https://atria.edu/home.php"
-          className=" text-red-600 underline"
-        >
-          Atria IT
-        </a>{" "}
-        x MCA Dept
-      </div>
+    <main>
+      <div className="w-screen h-screen">
+        <BackgroundGradientAnimation></BackgroundGradientAnimation>
+        <div className="w-full h-full absolute top-0 flex flex-col justify-center items-center">
+          <nav className=" absolute top-0 md:top-4 lg:top-4 w-screen md:w-[70vw] lg:w-[60vw] h-14 backdrop:blur-xs md:rounded-3xl px-4 py-2 border-solid md:border-white/5 md:border-[2px]  bg-slate-50/5 flex flex-row justify-between items-center">
+            <div id="leftSec">left</div>
+            <div id="rightSec">right</div>
+          </nav>
 
-      <div className=" absolute bottom-0 flex flex-col justify-center items-center mb-6">
-        <div className=" text-sm mb-4 opacity-45">In sponsorship with</div>
-        <div className="flex flex-row items-center justify-center gap-6">
-          <ImageH
-            src="/DEVFOLIO.png"
-            alt="DEVFOLIO LOGO"
-            width={100}
-            height={50}
-           
-            herf="https://devfolio.co" // Loads the image faster
-          />
-          <ImageH
-            src="/POLYGON.png"
-            alt="POLYGON LOGO"
-            width={100}
-            height={50}
-            herf="https://polygon.technology/"
-          />
-          <ImageH src="/ETH.png"
-            alt="ETHINDIA LOGO"
-            width={100}
-            height={50} herf="https://ethindia.co" />
+          <div className="w-auto h-auto flex flex-col text-white items-center justify-center">
+            <p className=" text-xs mb-2 opacity-60">GDG · On Campus Atria IT x MCA Dept Presents</p>
+            <h1 className=" text-9xl font-bold">Cepheus</h1>
+            <p className=" text-lg font-medium mt-4">Code. Build. Debug. Collaborate. Innovate. Compete. Conquer!</p>
+            <button className=" mt-8 px-6 py-4 bg-slate-400">Register Now!</button>
+          </div>
         </div>
       </div>
-    </div>
+    </main>
   );
 }
